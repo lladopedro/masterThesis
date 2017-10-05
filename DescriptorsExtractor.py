00007006a4b8b875d0e476b4bc307ab301ff8d9c
lladopedro <pedro.llado01@estudiant.upf.edu>
2017-10-05 16:55:20 +0200

    DescriptorsExtractor.py

--- DescriptorsExtractor.py
+++ DescriptorsExtractor.py
@@ -4,23 +4,24 @@ import json
 from essentia.standard import *
 from essentia import pool
 import numpy as np
+import csv
 
 
 
 def fetchFiles(inputDir = '/home/pedro/TFM/ZOOM0006/', fileType=".wav"):
-	ListOfFiles = []
-	for path,dir_names,files_names in os.walk(inputDir):
-		for file_name in files_names:
-		    if fileType in file_name.lower():
-		        ListOfFiles.append((path, file_name))
-	return(ListOfFiles)
+    ListOfFiles = []
+    for path,dir_names,files_names in os.walk(inputDir):
+        for file_name in files_names:
+            if fileType in file_name.lower():
+                ListOfFiles.append((path, file_name))
+    return(ListOfFiles)
 
 
 
 def extract_mfcc(ListOfFiles):
 
-	w = Windowing(type = 'hann')
-    spectrum = Spectrum()  
+    w = Windowing(type = 'hann')
+    spectrum = Spectrum()
     mfcc = MFCC()
 
     for path, file in ListOfFiles:
@@ -43,11 +44,55 @@ def extract_mfcc(ListOfFiles):
         aggrPool = PoolAggregator(defaultStats = [ 'mean', 'var' ])(pool)
         YamlOutput(filename = path + "/"+ file_name + ".json", format = "json")(aggrPool)
 
+#extractFeatures from one file
 
-def main():
-    data = fetchFiles(inputDir)
-    extract_mfcc(data)
+def extractFeatures(fileName):
+
+    extractor = Extractor(dynamics = True,
+                        dynamicsFrameSize = 88200,
+                        dynamicsHopSize = 44100,
+                        highLevel = True,
+                        lowLevel = True,
+                        lowLevelFrameSize = 4096,
+                        lowLevelHopSize = 2048,
+                        midLevel = True,
+                        namespace = "",
+                        relativeIoi = False,
+                        rhythm = True,
+                        sampleRate  = 44100,
+                        tonalFrameSize  = 4096,
+                        tonalHopSize = 2048,
+                        tuning = True)
+
+    JsonFile = '/home/pedro/TFM/ZOOM0006/resultTest.json'
+    JsonAggrFile = '/home/pedro/TFM/ZOOM0006/resultTestAggr.json'
+
+    #w = Windowing(type = 'hann') #only for the spectrum
+    #spectrum = Spectrum()  # FFT() would return the complex FFT, here we just want the magnitude spectrum
+
+    loader = essentia.standard.MonoLoader(filename = fileName)
+    audio = loader()
+    pool = essentia.Pool()
+    pool = extractor(audio)
+    #for frame in FrameGenerator(audio, frameSize = 4096, hopSize = 2048, startFromZero=False):
+    #    pool.add('lowLevel.spectrum', spectrum(w(frame)))
+
+    aggrPool = PoolAggregator(defaultStats = [ 'mean', 'var' ])(pool)
+    YamlOutput(filename = JsonFile, format = "json")(pool)
+    YamlOutput(filename = JsonAggrFile, format="json")(aggrPool)
+
+
+    ############ CSV
+    with open('/home/pedro/TFM/ZOOM0006/ZOOM0006_Tr1.csv', 'w') as file:
+        writer = csv.writer(file)
+        for item in pool.descriptorNames():
+            writer.writerow([pool[item]])
+    ############
+
+#def main():
+#    data = fetchFiles(inputDir)
+#    extract_mfcc(data)
 
 
 if __name__ == "__main__":
-main()
+    main()
