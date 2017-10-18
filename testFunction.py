f447e3c2519c4ec8f96801867991ee15a377675c
lladopedro <pedro.llado01@estudiant.upf.edu>
2017-10-18 20:30:27 +0200

    keep testing

--- testFunction.py
+++ testFunction.py
@@ -1,14 +1,32 @@
-#import essentia
-#from essentia.standard import *
-#from essentia import pool
 from OnsetDetectionFunc import OnsetDetectionF
+from dataExplorer import fetchFiles
 
-file = open("testWAVS/testFile.txt","w")
+#####	RETURN PATH AND FILES NAMES OF ALL THE ".WAV" OF THE DIRECTORY
 
-for alpha in 0.3, 0.4, 0.5, 0.6:
-	for silenceThreshold in 0.017, 0.02, 0.023, 0.026, 0.03:
-		OCP,realOS= OnsetDetectionF('../ZOOM0006/ZOOM0006_Tr1.WAV', alpha, silenceThreshold)
-		file.write("alpha = " + str(alpha) + "	silenceThreshold = " + str(silenceThreshold)+'\n' + str(OCP) + '\n'+ '\n')
-		file.write("REAL ONSETS alpha = " + str(alpha) + "	silenceThreshold = " + str(silenceThreshold)+'\n' + str(realOS) + '\n'+ '\n')
+#listOfFiles = fetchFiles("/home/pedro/tfm/dataBase/",".wav")
 
-file.close()
+
+#print listOfFiles[0][0] + '/' + listOfFiles[0][1]
+
+#####	Essentia.OnsetDetection PARAMETER {hfc, complex, complex_phase, flux, melflux, rms}, default = hfc)
+method = 'hfc'
+
+#####	Essentia.Onsets PARAMETERS
+alpha = 0.3
+silenceThreshold = 0.04
+
+
+#####	COMPUTE THE ONSETS
+
+#for alpha in 0.3, 0.4, 0.5, 0.6:
+#	for silenceThreshold in  0.03, 0.035, 0.04, 0.045, 0.05, 0.055:
+
+Onsets,realOS= OnsetDetectionF(listOfFiles[0][0] + '/' + listOfFiles[0][1], method, alpha, silenceThreshold)
+
+
+##### WRITE IN A TXT THE ONSETS DETECTED (PUT INTO THE SAME FOR UPWARDS IF NEEDED)
+
+#file = open("results/testFile%s.txt" %method,"w")
+#file.write("REAL ONSETS " +str(method) + " alpha = "  + str(alpha) + "	silenceThreshold = " + str(silenceThreshold)+'\n' + str(Onsets) + '\n'+ '\n')
+#file.write("REAL ONSETS " +str(method) + " alpha = "  + str(alpha) + "	silenceThreshold = " + str(silenceThreshold)+'\n' + str(realOS) + '\n'+ '\n')
+#file.close()
