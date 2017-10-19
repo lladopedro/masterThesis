be747860533d339ee70ddc9a655f3c6b8c89a436
lladopedro <pedro.llado01@estudiant.upf.edu>
2017-10-19 17:11:54 +0200

    working for the whole DB

--- testFunction.py
+++ testFunction.py
@@ -3,7 +3,7 @@ from dataExplorer import fetchFiles
 
 #####	RETURN PATH AND FILES NAMES OF ALL THE ".WAV" OF THE DIRECTORY
 
-#listOfFiles = fetchFiles("/home/pedro/tfm/dataBase/",".wav")
+listOfFiles = fetchFiles("/home/pedro/tfm/dataBase/",".wav")
 
 
 #print listOfFiles[0][0] + '/' + listOfFiles[0][1]
@@ -20,8 +20,8 @@ silenceThreshold = 0.04
 
 #for alpha in 0.3, 0.4, 0.5, 0.6:
 #	for silenceThreshold in  0.03, 0.035, 0.04, 0.045, 0.05, 0.055:
-
-Onsets,realOS= OnsetDetectionF(listOfFiles[0][0] + '/' + listOfFiles[0][1], method, alpha, silenceThreshold)
+for i in range (0,len(listOfFiles)):
+	Onsets,realOS= OnsetDetectionF(listOfFiles[i][0] + '/' + listOfFiles[i][1], method, alpha, silenceThreshold)
 
 
 ##### WRITE IN A TXT THE ONSETS DETECTED (PUT INTO THE SAME FOR UPWARDS IF NEEDED)
