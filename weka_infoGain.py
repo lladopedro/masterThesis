d9c1abcc0b457c6fc4f87b296b122651a33d7ff9
lladopedro <pedro.llado01@estudiant.upf.edu>
2018-05-14 21:48:32 +0200

    writing data to csv

--- weka_infoGain.py
+++ weka_infoGain.py
@@ -2,9 +2,11 @@ import sys
 sys.path.append("/usr/share/java/ij.jar")
 from ij import *
 from ij.gui import Plot
+import csv
 import java.io.FileReader as FileReader
 import weka.core.Instances as Instances
-import weka.core.converters.ConverterUtils.DataSource as DS
+import weka.core.converters.ConverterUtils.DataSource as DSave
+import weka.core.converters.ConverterUtils.DataSink as DSink
 from weka.classifiers import Classifier, SingleClassifierEnhancer, MultipleClassifiersCombiner #PredictionOutput, Kernel, KernelClassifier
 import weka.classifiers.meta.FilteredClassifier
 from weka.classifiers import Evaluation
@@ -14,6 +16,8 @@ import weka.attributeSelection.ASEvaluation as ASEvaluation
 import weka.attributeSelection.ASSearch as ASSearch
 from jarray import array
 from java.awt import Color
+#import java.io.Serializable.Saver.setDir
+import weka.core.converters.AbstractSaver
 
 #import weka.plot.classifiers as plot_cls
 import weka.gui.visualize as visualize
@@ -24,11 +28,11 @@ import weka.gui.visualize as visualize
 
 print "Loading data..."
 # load data
-file = DS.read("/home/pedro/tfm/dataBaseFull/CorrectedAudioSofia/zzzWEKA/ZOOM00089_featSel.csv")
+file = DSave.read("/home/pedro/tfm/dataBaseFull/CorrectedAudioSofia/zzzWEKA/ZOOM00089_featSel.csv")
 data = Instances(file)
 
 numAttr = data.numAttributes()
-nAt = 15     #number of Attributes we want to test now
+maxNumAt = 15     #number of Attributes we want to test now
    
 ##### set the class Index - the index of the dependent variable
 data.setClassIndex(data.numAttributes() - 1)
@@ -40,12 +44,21 @@ removeType = weka.filters.unsupervised.attribute.RemoveType()
 
 ##### We append the desired classifiers in MyClassifier array
 
+
 MyClassifier = []
 
 #MyClassifier.append(weka.classifiers.rules.ZeroR())
-#MyClassifier.append(weka.classifiers.rules.OneR())
+MyClassifier.append(weka.classifiers.rules.OneR())
 MyClassifier.append(weka.classifiers.trees.J48())
+print (MyClassifier[0].getClass())
 
+print ()
+
+my_file = open('/home/pedro/Descargas/test.csv','w')
+headers = []
+for j in range(0,maxNumAt): headers.append("Acc w/ %d att." %j)
+CSVtitle = "Classifier", headers
+my_file.write(str(CSVtitle) + "\n")
 
 
 ##### We evaluate all the desired classifiers with different numbers of attributes to quantify the attribute selection and decide how many attributes we do need
@@ -54,7 +67,7 @@ for i in range(0,len(MyClassifier)): #Computing with each classifier
     
     infoGainCurve = []               #Array for infoGain curve representation
     
-    for x in range(1,nAt+1):    #numAttr-1):      #Computing the infoGain depending on the number of Attributes
+    for x in range(1,maxNumAt+1):    #numAttr-1):      #Computing the infoGain depending on the number of Attributes
         
         #We use a Attribute Selected Classifier
         AtSelClsf = weka.classifiers.meta.AttributeSelectedClassifier()
@@ -69,7 +82,7 @@ for i in range(0,len(MyClassifier)): #Computing with each classifier
         FiltClsf = weka.classifiers.meta.FilteredClassifier()                       
         FiltClsf.filter = removeType
         FiltClsf.classifier = AtSelClsf
-        print (FiltClsf)
+        #print (FiltClsf)
         
         ##### Evaluation of the model with crossvalidation
         
@@ -79,30 +92,39 @@ for i in range(0,len(MyClassifier)): #Computing with each classifier
         ##### Printing results
         
         print(str(evl.pctCorrect()) + " % correct with " + str(x) + " attributes.\n")
-        #print(evl.toSummaryString())
+        print(evl.toSummaryString())
         #print(evl.toClassDetailsString())
         print(evl.toMatrixString())
-        #print (evl.getHeader())
+        
         
         infoGainCurve.append(evl.pctCorrect())
     print (infoGainCurve)
     
-    #xArr1 = array([0.9, 2.0, 3.14], 'd')
-    #yArr = array([2.3, 2.0, 13.14], 'd')
+
     x_val = []
-    for i in range (1,nAt+1):x_val.append(i)
+    for k in range (1,maxNumAt+1):x_val.append(k)
     
-    #xArr = array([1,2,3], 'd')
     xArr = array(x_val, 'd')
     
     yArr = array(infoGainCurve,'d') 
     
-    plot = Plot("Title", "X", "Y")
+    plot = Plot("Info gain by number of attributes", "Number of Attributes", "Accuracy %")
     plot.setLimits(0.0, numAttr+1 ,60, 100.0)
     plot.setColor(Color.RED)
-    #plot.addPoints(xArr,yArr, Plot.LINE)
     plot.addPoints(xArr,yArr, Plot.LINE)
     
-    plot.show()
+    #plot.show()
     
+
+    my_file.write(str(MyClassifier[i].getClass()).split("'")[1] + "," + str(infoGainCurve) + "\n")
     
+
+    
+
+    #print(data.type())
+    #print(output.type())
+    #SaveFile = DSink("/home/pedro/Descargas/test.csv")
+    #SaveFile.write(output)
+    #DSink.main(["/home/pedro/Descargas/test.csv", str(infoGainCurve)])
+    
+my_file.close()
